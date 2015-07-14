25b969daa96f245099687e9124551abec2f83352
Lukas Obletter da Cudan <developers@skirentalresorts.com>
2015-07-14 14:25:31 +0200

    wrapper identifier for every view

--- www/app/components/help/help-controllers.js
+++ www/app/components/help/help-controllers.js
@@ -7,6 +7,7 @@ var helpControllers = angular.module('helpControllers', []);
 helpControllers.controller('helpCtrl', 
 						   ['$scope', 
                     function($scope) {
+							   
 
 				
 }])
