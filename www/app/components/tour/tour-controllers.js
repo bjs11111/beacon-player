f8c2434230bb19f48f8d306bbca5072e605cd2d8
Lukas Obletter da Cudan <developers@skirentalresorts.com>
2015-07-14 18:27:39 +0200

    Tour View perfectioning

--- www/app/components/tour/tour-controllers.js
+++ www/app/components/tour/tour-controllers.js
@@ -34,7 +34,11 @@ tourControllers.controller( 'tourCtrl', [ '$scope',
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.contentThumbnailUrl="http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.triggerZone="Near";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].sort=20;
-		
+		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeaconKey=
+				$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.uuid + "." + 
+				$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.major + "." + 
+				$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.minor;
+				
 		
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.2']={scanData :{}};
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.2'].scanData.rssi=-75;
@@ -46,6 +50,10 @@ tourControllers.controller( 'tourCtrl', [ '$scope',
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.2'].bcmsBeacon.contentThumbnailUrl="http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.2'].bcmsBeacon.triggerZone="Far";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].sort=10;
+		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeaconKey=
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.uuid + "." + 
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.major + "." + 
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.minor;
 		
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.3']={scanData :{}};
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.3'].scanData.rssi=-75;
@@ -57,6 +65,10 @@ tourControllers.controller( 'tourCtrl', [ '$scope',
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.3'].bcmsBeacon.contentThumbnailUrl="http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.3'].bcmsBeacon.triggerZone="Intermediate";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].sort=8;
+		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeaconKey=
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.uuid + "." + 
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.major + "." + 
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.minor;
 		
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.4']={scanData :{}};
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.4'].scanData.rssi=-75;
@@ -68,6 +80,10 @@ tourControllers.controller( 'tourCtrl', [ '$scope',
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.4'].bcmsBeacon.contentThumbnailUrl="http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.4'].bcmsBeacon.triggerZone="Far";
 		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].sort=7;
+		$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeaconKey=
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.uuid + "." + 
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.major + "." + 
+			$scope.receivedDevicesList['699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1'].bcmsBeacon.minor;
 		
 
 }]);
