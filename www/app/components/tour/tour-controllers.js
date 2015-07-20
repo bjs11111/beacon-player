5e36b56172b87109f5cee2ce1025899df40e7080
Lukas Obletter da Cudan <developers@skirentalresorts.com>
2015-07-20 14:36:16 +0200

    Whitelist and content connected with view

--- www/app/components/tour/tour-controllers.js
+++ www/app/components/tour/tour-controllers.js
@@ -30,11 +30,18 @@ tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScanne
 	$scope.bleDevicesCtrl.allDevicesList = {};	
 	$scope.bleDevicesCtrl.filteredDevicesList = {};
 	
+	//we need a "." in our view variables
+	$scope.apiDevicesCtrl = {};
+	$scope.apiDevicesCtrl.apiDevicesList = {};
+	
+	var secondsLastViewUpdate = 0;
+	var MIN_VIEW_UPDATE_INTERVAL = 2;
+	
 	
 	$scope.openIABWithKey = generalService.openIABWithKey;
 
 	$scope.showDeviceList=[
-		                            {	bcmsBeaconKey:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1",
+		                           /* {	bcmsBeaconKey:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1",
 		                            	contentTitle:"Beacon1",
 		                            	contentThumbnailUrl:"http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF",
 		                            	uuid:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012",
@@ -73,19 +80,125 @@ tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScanne
 		                            	triggerZone:"Near",
 		                            	rssi:-65,
 		                            	sort:1
-		                            }
+		                            }*/
 		];
 	
 	
 	
+	
+	//TODO: Put into the Device Controler
+	
+
+
+  	//start refreshes serverdata every x ms
+	$scope.apiDevicesCtrl.refreshServerData = function() {
+		beaconAPIService.getAllBeacons()
+	    	.then(
+    			//success
+    			function (apiDeviceList) { 
+    				console.log('apiDevicesListCtrl refreshServerData' + Date.now() ); 
+    				angular.forEach(apiDeviceList, function(beacon, key) {
+    					$scope.apiDevicesCtrl.apiDevicesList[key] = {	bcmsBeaconKey	: key,
+    																	bcmsBeacon 		: beacon
+    																};
+    				});	
+    				
+    				$scope.$broadcast('scroll.refreshComplete');  }, 
+    			//error
+    			function() { $scope.$broadcast('scroll.refreshComplete'); }); 
+	}	
+	
+	
+	
+	
+	
+	
+	// Filters only known Devices
 	var filterKnownBeacons = function(allDevicesList){
 		console.log("Filter known Devices:" + allDevicesList);
+		var onlyKnownDevicesList = {};
+		
+		// Filter Only known Devices
 		for (var key in allDevicesList) { 
-			if(bleDeviceService.getKnownDevice(key)){
+			//TODO: universalize key format. Somewhere it is with "-" and sometimes without.
+			var key2=allDevicesList[key].iBeaconUuid + "." + allDevicesList[key].major + "." + allDevicesList[key].minor;
+			var tmpDevice= $scope.apiDevicesCtrl.apiDevicesList[key2];
+			if(tmpDevice){
 				console.log("Beacon Found! ");
+				onlyKnownDevicesList[key2]=tmpDevice;
 			}
 		}
+		
+		return onlyKnownDevicesList;
 	}
+	
+	
+	var updateIonicView = function(filteredDevicesList){
+		var tmpDeviceList=[];
+		
+		for(var key in filteredDevicesList){
+			
+			var deviceToAdd ={
+					bcmsBeaconKey:filteredDevicesList[key].bcmsBeaconKey,
+			    	contentTitle:filteredDevicesList[key].bcmsBeacon.contentTitle,
+			    	contentThumbnailUrl:filteredDevicesList[key].bcmsBeacon.contentThumbnailUrl,
+			    	uuid:filteredDevicesList[key].bcmsBeacon.iBeaconUuid,
+			    	major:filteredDevicesList[key].bcmsBeacon.major,
+			    	minor:filteredDevicesList[key].bcmsBeacon.minor,		                            	
+			    	triggerZone:filteredDevicesList[key].bcmsBeacon.triggerZone,
+			    	rssi:-65, 
+			    	sort:4
+			};
+			tmpDeviceList.push(deviceToAdd);
+		}
+		
+		
+		
+		
+    	
+    	$scope.showDeviceList=tmpDeviceList;
+		
+	}
+	
+	
+	// Filters only known Devices
+	var filterWhitelistBeacons = function(filteredDevicesList){
+		console.log("Filter Whitelist:" + filteredDevicesList);
+		
+		var whitelistDevicesList = {};
+		var whitelistBeacons = {};
+		
+		// Look if there are Whitelist Beacons and remember the user ids
+		for (var key in filteredDevicesList) { 
+			//TODO: universalize key format. Somewhere it is with "-" and sometimes without.
+			var tmpDevice = $scope.apiDevicesCtrl.apiDevicesList[key];
+			console.log(tmpDevice);
+			if(tmpDevice.bcmsBeacon.whitelisted==1){
+				whitelistBeacons[key]=tmpDevice;
+			}
+		}
+		
+		// Only add Beacons that have the same user id as the whitelist beacons
+		for(var key in filteredDevicesList){
+			var tmpDevice = $scope.apiDevicesCtrl.apiDevicesList[key];
+			
+			if(Object.keys(whitelistBeacons).length==0){
+				whitelistDevicesList[key]=tmpDevice; // If no Whitelist beacon is in the area, add it anyways to list
+			}
+			else{
+				for(var key2 in whitelistBeacons){
+					if(whitelistBeacons[key2].bcmsBeacon.userId==tmpDevice.bcmsBeacon.userId)
+						whitelistDevicesList[key]=tmpDevice;
+				}
+			}
+			
+		}
+		
+		
+		return whitelistDevicesList;
+	}
+	
+	
 	
 	
 	//this is used to update list after serverdata updated   	
@@ -100,15 +213,20 @@ tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScanne
 		$scope.bleDevicesCtrl.filteredDevicesList = filterKnownBeacons($scope.bleDevicesCtrl.allDevicesList);
 		
 		// Filter Devices that with Whitelist Beacons
-		
+		$scope.bleDevicesCtrl.filteredDevicesList = filterWhitelistBeacons($scope.bleDevicesCtrl.filteredDevicesList);
 		
 		// Calculate Distance Beacons <--> Phone
 		
 		
 		// Update Ionic View Array
+		updateIonicView($scope.bleDevicesCtrl.filteredDevicesList);
 		
-		
-		//$scope.$apply();
+		if(secondsLastViewUpdate + MIN_VIEW_UPDATE_INTERVAL < new Date().getTime() / 1000)
+		{
+			secondsLastViewUpdate = new Date().getTime() / 1000;
+			$scope.$apply();
+			
+		}
 		
 	};
 	
@@ -122,33 +240,6 @@ tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScanne
 
 		
 	// Get Updates from Bluetooth Scan
-		
-
-	
-	
-	
-	
-	
-	$scope.bleDevicesCtrl = {};
-	$scope.bleDevicesCtrl.allDevicesList = {};
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 
