754469833e737121222240e0393120c12983124b
Michael Hladky <michael@hladky.at>
2015-07-22 23:25:34 +0200

    filters

---
+++ www/app/common/services/TourListManagers-services.js
@@ -0,0 +1,195 @@
+/* Services */
+var tourListManagers = angular.module('tourListManagers', ['bleFilters', 'bleChannels', 'beaconAPIServices']);
+
+tourListManagers.constant( "bleDeviceChannelConfig", {
+	//events
+	displayDeviceDeviceUpdates		: 'displayDeviceDeviceUpdates',
+	displayDeviceDeviceUpdates 		: 'displayDeviceDeviceUpdates',
+});
+
+//bleDeviceChannel 
+tourListManagers.factory('bleDeviceChannel', 
+		['$rootScope', 'bleDeviceChannelConfig',
+function ($rootScope,   bleDeviceChannelConfig) {
+   
+	//private functions
+	var _subscribe = function(event, $scope, scopeHandler, mapArgs) {
+		//subscribe with rotscope to event and cache unsubscribe function
+		var unsubscopeHandler = $rootScope.$on(event, function(event, args) {
+			scopeHandler(mapArgs(args));
+		 });
+		 
+		//unsubscribe rootRcope listener after scope destruction
+		$scope.$on('$destroy', function() {
+			unsubscopeHandler();
+		});
+	};
+	
+	var _publish = function(event, args) {
+		 $rootScope.$emit(event, args);
+	};
+			
+   // publish knownDevices updated notification
+   var pubKnownDeviceUpdated = function (knownDeviceKey) {
+	   _publish(bleDeviceChannelConfig.bleDeviceUpdates, {knownDeviceKey : knownDeviceKey});
+   };
+   // subscribe to knownDevices updated notification
+   var subKnownDeviceUpdated = function ($scope, scopeHandler) {
+	   console.log('subKnownDeviceUpdated');
+	   _subscribe(bleDeviceChannelConfig.bleDeviceUpdates, $scope, scopeHandler, function(args) { return args.knownDeviceKey; });
+   };
+   
+// publish knownDevices updated notification
+   var pubKnownDevicesUpdated = function (knownDeviceKeys) {
+	   _publish(bleDevicesChannelConfig.bleDevicesUpdates, {knownDeviceKeys : knownDeviceKeys});
+   };
+   // subscribe to knownDevices updated notification
+   var subKnownDevicesUpdated = function ($scope, scopeHandler) {
+	   console.log('subKnownDevicesUpdated');
+	   _subscribe(bleDevicesChannelConfig.bleDevicesUpdates, $scope, scopeHandler, function(args) { return args.knownDeviceKeys; });
+   };
+   
+   // return the publicly accessible methods
+   return {
+	   subKnownDeviceUpdated 			: subKnownDeviceUpdated,
+	   pubKnownDeviceUpdated			: pubKnownDeviceUpdated,
+	   
+	   subKnownDevicesUpdated 			: subKnownDevicesUpdated,
+	   pubKnownDevicesUpdated			: pubKnownDevicesUpdated,
+   	};
+}]);
+
+tourListManagers.constant( "bleDeviceServiceConfig", {
+	mapTypeBleDevice	: 'scanData',
+	mapTypeAPIDevice	: 'bcmsBeacon',
+	
+	triggerAreas : {
+		positive 		: 'Positive', 
+		negative 		: 'Negative', 
+		outOfRange 		: 'OutOfRange'
+	},
+	triggerZones: {
+		//Notice Name is same as in bcmsBeaconData
+		near 			: { name :	'Near',
+							entryThresholdOffset 	: 20,
+							exitThresholdOffset 	: 10
+						  },
+		intermediate 	: { name :	'Intermediate',
+							entryThresholdOffset 	: 10,
+							exitThresholdOffset 	: -10
+						  },
+		far 			: { name :	'Far',
+							entryThresholdOffset 	: -10,
+							exitThresholdOffset 	: -40
+						  },
+	},
+	
+});
+
+
+/*http and bel data mapper*/
+tourListManagers.factory('bleDeviceService', 
+		['$rootScope', '$q', '$filter', 'bleDeviceServiceConfig', 'bleDeviceChannel', 'bleScannerChannel', 'beaconAPIService', 'beaconAPIChannel',        
+function( $rootScope,   $q,   $filter,   bleDeviceServiceConfig,   bleDeviceChannel,   bleScannerChannel,   beaconAPIService,   beaconAPIChannel){
+	 		  // $new(true) => isolate scope
+			  var 	scope = $rootScope.$new(), 
+         	  // list of all scanned devices [ iBeaconUuid.Major.Minor => deviceData, ]
+         	  		knownDevicesList = [],
+         	  //
+         	  		bcmsBeaconKeyToObj  = $filter('bcmsBeaconKeyToObj');
+         	
+			 var subGetAllBeaconsHandler =  function(beaconList)  {
+         		 angular.forEach(beaconList, function(beacon, key) {
+ 					mapBeaconDataToKnownDevices(beacon, bleDeviceServiceConfig.mapTypeAPIDevice); 
+ 				});	
+	         };
+	         	  
+			  
+              // triggers getAllBeacons request
+              var updateBeaconsFromServer = function() {
+            	  var defer = $q.defer();
+            	  
+            	  beaconAPIService
+            	  .getAllBeacons().then( 
+            			  function(beaconList) {
+		            		  angular.forEach(beaconList, function(beacon, key) {
+		       				     mapBeaconDataToKnownDevices(beacon, bleDeviceServiceConfig.mapTypeAPIDevice); 
+		       				  });	
+		            		  defer.resolve();
+		            	  },
+		            	  function() {
+		            		  defer.reject();
+		            	  }
+            	  ); 
+            	  
+            		
+  				   return defer.promise;
+              };
+			  
+               // returns the array of knownDevices
+               var getKnownDevices = function() {
+             	  return knownDevicesList; 
+               };
+               
+               var getKnownDevice = function(cmsBeaconKey) {
+             	  var searchedDevice = knownDevicesList[cmsBeaconKey];
+             	  if(searchedDevice) { return searchedDevice; }
+             	  return false; 
+               };
+               
+               //this function holds all logic for updating and interpreting data form scanner and server
+               var mapBeaconDataToKnownDevices = function(deviceData, type) {
+             	  type = (type)?type:false;
+             	
+             	  var 	bcmsBeaconKey  = deviceData.iBeaconUuid+'.'+deviceData.major+'.'+deviceData.minor, 
+             	  		inTriggerRange  = false,
+             	  		data = knownDevicesList[bcmsBeaconKey];
+             	 
+             	  //@TODO abstract and move this mapping
+             	  //init
+             	  if( !data ) {
+             		  data = {};
+         			  data.bcmsBeaconKey = bcmsBeaconKey;
+             	  }  
+             	
+             	  //device from ble scanner
+         		  if ( type == bleDeviceServiceConfig.mapTypeBleDevice ) {
+         			  
+         			  data.scanData = deviceData;
+         			  knownDevicesList[bcmsBeaconKey] = data;
+         			  
+             	  } 
+             	  //bcmsData
+             	  else if ( bleDeviceServiceConfig.mapTypeAPIDevice ) {
+             		  data.bcmsBeacon = deviceData;
+             		  knownDevicesList[bcmsBeaconKey] = data;
+             	  } else {
+             		  console.log('check this!'); 
+             		  return;
+             	  }
+         		 
+         		  bleDeviceChannel.pubKnownDeviceUpdated(bcmsBeaconKey);
+         		
+               }
+               
+         	  var onFoundBleDeviceHandler = function(rawDevice)  {
+         		 mapBeaconDataToKnownDevices(rawDevice, bleDeviceServiceConfig.mapTypeBleDevice);  
+         	  };
+         	  
+         	  var init = function() {
+         		bleScannerChannel.onFoundBleDevice(scope, onFoundBleDeviceHandler); 
+         	  };
+               
+               //do initialisation 
+               init();
+               
+               // return the public accessible methods
+               return {
+            	  updateBeaconsFromServer		: updateBeaconsFromServer,
+             	  getKnownDevices				: getKnownDevices,
+             	  getKnownDevice				: getKnownDevice,
+             	  mapBeaconDataToKnownDevices	: mapBeaconDataToKnownDevices,
+             	  //calculateActualTriggerArea	: calculateActualTriggerArea
+               };
+     
+}]);   
--- www/app/components/tour/tour-controllers.js
+++ www/app/components/tour/tour-controllers.js
@@ -1,5 +1,5 @@
 /* Controllers of start component */
-var bleDevicesControllers = angular.module('bleDevicesControllers', ['bleChannels', 'deviceManagers', 'beaconAPIServices', 'deviceListDirectives']);
+var bleDevicesControllers = angular.module('bleDevicesControllers', ['listFilters', 'bleChannels', 'deviceManagers', 'beaconAPIServices', 'deviceListDirectives']);
  
 
 
@@ -10,101 +10,72 @@ var tourControllers = angular.module('tourControllers', ['bleChannels', 'deviceM
 
 /* Tour Controllers Config*/
 tourControllers.constant("tourCtrlConfig", {
+	//
+	MIN_VIEW_UPDATE_INTERVAL : 2,
+	MAX_MEASUREMENTS:15,	// Maximum Number of RSSI measurements to calculate average distance
+	MAX_TIME:5,				// Maximum Time a measurement is preserved to average Distance
+	FACTOR_PER_SECOND:0.85,	// Time weighted average factor 
 	
-	iabOpenVibratePattern : [100, 100, 100],
-	iabOpenVibrateTime : 100,
-
-	iabDefaultSettings 		:  {
-		      location		: 'no',
-		      clearcache	: 'no',
-		      toolbar		: 'no'
-		    }
+	//
+	OFFSET_PROXIMITY_NEAR:0,
+	OFFSET_PROXIMITY_INTERMEDIATE:15,
+	OFFSET_PROXIMITY_FAR:30,
 });
 
 /* Tour Controllers */
-tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScannerChannel','beaconAPIService', 'bleScannerChannel', 'bleDeviceService',
-                                  function($scope,   generalService, bleScannerChannel,  beaconAPIService,   bleScannerChannel, bleDeviceService) {
+tourControllers.controller( 'tourCtrl', 
+		['$scope', '$filter', 'tourCtrlConfig', 'generalService', 'bleScannerChannel','beaconAPIService', 'bleScannerChannel', 
+function( $scope,   $filter,   tourCtrlConfig,   generalService,   bleScannerChannel,  beaconAPIService,   bleScannerChannel ) {
 	
-	
-	$scope.bleDevicesCtrl = {};
-	$scope.bleDevicesCtrl.allDevicesList = {};	
-	$scope.bleDevicesCtrl.filteredDevicesList = {};
-	
-	//we need a "." in our view variables
-	$scope.apiDevicesCtrl = {};
-	$scope.apiDevicesCtrl.apiDevicesList = {};
 	
 	var secondsLastViewUpdate = 0;
-	var MIN_VIEW_UPDATE_INTERVAL = 2;
-	var MAX_MEASUREMENTS=15;	// Maximum Number of RSSI measurements to calculate average distance
-	var MAX_TIME=5;				// Maximum Time a measurement is preserved to average Distance
-	var FACTOR_PER_SECOND=0.85;	// Time weighted average factor 
-	var OFFSET_PROXIMITY_NEAR=0;
-	var OFFSET_PROXIMITY_INTERMEDIATE=15;
-	var OFFSET_PROXIMITY_FAR=30;
+	var knownBeaconsFilter = $filter('knownBeaconsFilter');
+	var whitelistBeaconsFilter = $filter('whitelistBeaconsFilter');
+	var lastScanFilter =  $filter('lastScanFilter');
 	
+	//values
+	$scope.tourCtrlData = {};
+	$scope.tourCtrlData.allDevicesList = {};	
+	$scope.tourCtrlData.apiDevicesList = {};
+	$scope.tourCtrlData.filteredDevicesList = {};
+	$scope.tourCtrlData.showDeviceList = {};
 	
+	//functions
 	$scope.openIABWithKey = generalService.openIABWithKey;
 
-	$scope.showDeviceList=[
-		                           /* {	bcmsBeaconKey:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1",
-		                            	contentTitle:"Beacon1",
-		                            	contentThumbnailUrl:"http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF",
-		                            	uuid:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012",
-		                            	major:1,
-		                            	minor:1,		                            	
-		                            	triggerZone:"Near",
-		                            	rssi:-65,
-		                            	sort:4
-		                            },
-		                            {	bcmsBeaconKey:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1",
-		                            	contentTitle:"Beacon2",
-		                            	contentThumbnailUrl:"http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF",
-		                            	uuid:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012",
-		                            	major:1,
-		                            	minor:2,		                            	
-		                            	triggerZone:"Intermediate",
-		                            	rssi:-65,
-		                            	sort:2
-		                            },
-		                            {	bcmsBeaconKey:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1",
-		                            	contentTitle:"Beacon3",
-		                            	contentThumbnailUrl:"http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF",
-		                            	uuid:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012",
-		                            	major:1,
-		                            	minor:3,		                            	
-		                            	triggerZone:"Near",
-		                            	rssi:-65,
-		                            	sort:3
-		                            },
-		                            {	bcmsBeaconKey:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012.1.1",
-		                            	contentTitle:"Beacon4",
-		                            	contentThumbnailUrl:"http://www.starnberger.at/dev-bcms/sites/default/files/styles/thumblail_cut_100_100/public/content/bild/108/108771-em-ble-beaconblebeacon.jpg?itok=MqqPjhUF",
-		                            	uuid:"699EBC80-E1F3-11E3-9A0F-0CF3EE3BC012",
-		                            	major:1,
-		                            	minor:4,		                            	
-		                            	triggerZone:"Near",
-		                            	rssi:-65,
-		                            	sort:1
-		                            }*/
-		];
-	
-	
-	
-	
+	//start interval for cleaning old devices
+	/*startcleaningOldDevicesinterval = function (interval) {
+		//console.log('APPTEST: on startcleaningOldDevicesinterval');
+		if(!$scope.cleaningOldDevicesintervalPromise) cleaningOldDevicesintervalPromise = $interval(function() {
+				for (key in $scope.receivedDevicesList) {
+					if($scope.receivedDevicesList[key].scanData.lastScan < Date.now() -  interval) {
+						delete $scope.receivedDevicesList[key];
+					} 
+				}
+				$scope.updateListLength();
+			}, interval);
+	};
+	//stopt interval for cleaning old devices
+	stopcleaningOldDevicesinterval = function () {
+		if($scope.cleaningOldDevicesintervalPromise) {
+			$interval.cancel(intervalPromise);
+			$scope.cleaningOldDevicesintervalPromise = undefined;
+		}
+	};*/
+
 	//TODO: Put into the Device Controler
 	
 
 
   	//start refreshes serverdata every x ms
-	$scope.apiDevicesCtrl.refreshServerData = function() {
+	$scope.tourCtrlData.refreshServerData = function() {
 		beaconAPIService.getAllBeacons()
 	    	.then(
     			//success
     			function (apiDeviceList) { 
     				
     				angular.forEach(apiDeviceList, function(beacon, key) {
-    					$scope.apiDevicesCtrl.apiDevicesList[key] = {	bcmsBeaconKey	: key,
+    					$scope.tourCtrlData.apiDevicesList[key] = {	bcmsBeaconKey	: key,
     																	bcmsBeacon 		: beacon
     																};
     				});	
@@ -116,32 +87,12 @@ tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScanne
 	
 	
 	
-	
-	
-	
-	// Filters only known Devices
-	var filterKnownBeacons = function(allDevicesList){
-		
-		var onlyKnownDevicesList = {};
-		
-		// Filter Only known Devices
-		for (var key in allDevicesList) { 
-			var tmpDevice= $scope.apiDevicesCtrl.apiDevicesList[key];
-			if(tmpDevice){
-				onlyKnownDevicesList[key]=tmpDevice;
-			}
-		}
-		
-		return onlyKnownDevicesList;
-	}
-	
-	
 	var updateIonicView = function(filteredDevicesList){
 		var tmpDeviceList=[];
 		
 		for(var key in filteredDevicesList){
 			
-			var deviceToAdd ={
+			var deviceToAdd = {
 					bcmsBeaconKey:filteredDevicesList[key].bcmsBeaconKey,
 			    	contentTitle:filteredDevicesList[key].bcmsBeacon.contentTitle,
 			    	contentThumbnailUrl:filteredDevicesList[key].bcmsBeacon.contentThumbnailUrl,
@@ -149,113 +100,65 @@ tourControllers.controller( 'tourCtrl', [ '$scope', 'generalService', 'bleScanne
 			    	major:filteredDevicesList[key].bcmsBeacon.major,
 			    	minor:filteredDevicesList[key].bcmsBeacon.minor,		                            	
 			    	triggerZone:filteredDevicesList[key].bcmsBeacon.triggerZone,
-			    	rssi:$scope.bleDevicesCtrl.allDevicesList[key].rssi,
+			    	rssi:$scope.tourCtrlData.allDevicesList[key].rssi,
 			    	sort:filteredDevicesList[key].sort
 			};
 			tmpDeviceList.push(deviceToAdd);
 		}
-		
-		
-		
-		
     	
-    	$scope.showDeviceList=tmpDeviceList;
+    	$scope.tourCtrlData.showDeviceList=tmpDeviceList;
 		
 	}
-	
-	
-	// Filters only known Devices
-	var filterWhitelistBeacons = function(filteredDevicesList){
-		
-		var whitelistDevicesList = {};
-		var whitelistBeacons = {};
-		
-		// Look if there are Whitelist Beacons and remember the user ids
-		for (var key in filteredDevicesList) { 
-			//TODO: universalize key format. Somewhere it is with "-" and sometimes without.
-			var tmpDevice = $scope.apiDevicesCtrl.apiDevicesList[key];
-			
-			if(tmpDevice.bcmsBeacon.whitelisted==1){
-				whitelistBeacons[key]=tmpDevice;
-			}
-		}
-		
-		// Only add Beacons that have the same user id as the whitelist beacons
-		for(var key in filteredDevicesList){
-			var tmpDevice = $scope.apiDevicesCtrl.apiDevicesList[key];
-			
-			if(Object.keys(whitelistBeacons).length==0){
-				whitelistDevicesList[key]=tmpDevice; // If no Whitelist beacon is in the area, add it anyways to list
-			}
-			else{
-				for(var key2 in whitelistBeacons){
-					if(whitelistBeacons[key2].bcmsBeacon.userId==tmpDevice.bcmsBeacon.userId)
-						whitelistDevicesList[key]=tmpDevice;
-				}
-			}
-			
-		}
-		
-		
-		return whitelistDevicesList;
-	}
-	
-	
+
 	//TODO: Implement detailed Distance estimation
 	var calculateDistance = function(filteredDevicesList){
 		for (var key in filteredDevicesList) { 
-			
-			var OFFSET_PROXIMITY_NEAR=0;
-			var OFFSET_PROXIMITY_INTERMEDIATE=15;
-			var OFFSET_PROXIMITY_FAR=30;
-			
-			if(filteredDevicesList[key].bcmsBeacon.triggerZone=="Near") filteredDevicesList[key].sort = (-1) * $scope.bleDevicesCtrl.allDevicesList[key].rssi - OFFSET_PROXIMITY_NEAR;
-			if(filteredDevicesList[key].bcmsBeacon.triggerZone=="Intermediate") filteredDevicesList[key].sort = (-1) * $scope.bleDevicesCtrl.allDevicesList[key].rssi - OFFSET_PROXIMITY_INTERMEDIATE;
-			if(filteredDevicesList[key].bcmsBeacon.triggerZone=="Far") filteredDevicesList[key].sort = (-1) * $scope.bleDevicesCtrl.allDevicesList[key].rssi - OFFSET_PROXIMITY_FAR;
-			
-			
+			if(filteredDevicesList[key].bcmsBeacon.triggerZone=="Near") filteredDevicesList[key].sort = (-1) * $scope.tourCtrlData.allDevicesList[key].rssi - tourCtrlConfig.OFFSET_PROXIMITY_NEAR;
+			if(filteredDevicesList[key].bcmsBeacon.triggerZone=="Intermediate") filteredDevicesList[key].sort = (-1) * $scope.tourCtrlData.allDevicesList[key].rssi - tourCtrlConfig.OFFSET_PROXIMITY_INTERMEDIATE;
+			if(filteredDevicesList[key].bcmsBeacon.triggerZone=="Far") filteredDevicesList[key].sort = (-1) * $scope.tourCtrlData.allDevicesList[key].rssi - tourCtrlConfig.OFFSET_PROXIMITY_FAR;
 		}
 		return filteredDevicesList;
-	}
-	
-	
-	
+	} 
 	
 	// Get Updates from Bluetooth Scan 	
 	var onFoundDeviceHandler = function(preparedDevice)  {
-
+ 
 		// Put Device into Array
-		$scope.bleDevicesCtrl.allDevicesList[preparedDevice.bcmsBeaconKey] 	= preparedDevice;
-		$scope.bleDevicesCtrl.filteredDevicesList = {};
+		$scope.tourCtrlData.allDevicesList[preparedDevice.bcmsBeaconKey] 	= preparedDevice;
+		$scope.tourCtrlData.filteredDevicesList = {};
 		
 		// Filter Devices that are in BCMS
-		$scope.bleDevicesCtrl.filteredDevicesList = filterKnownBeacons($scope.bleDevicesCtrl.allDevicesList);
+		$scope.tourCtrlData.filteredDevicesList = knownBeaconsFilter($scope.tourCtrlData.allDevicesList, $scope.tourCtrlData.apiDevicesList);
 		
 		// Filter Devices that with Whitelist Beacons
-		$scope.bleDevicesCtrl.filteredDevicesList = filterWhitelistBeacons($scope.bleDevicesCtrl.filteredDevicesList);
+		$scope.tourCtrlData.filteredDevicesList = whitelistBeaconsFilter($scope.tourCtrlData.filteredDevicesList,  $scope.tourCtrlData.apiDevicesList);
 		
 		// Calculate Distance Beacons <--> Phone
-		
-		$scope.bleDevicesCtrl.filteredDevicesList = calculateDistance($scope.bleDevicesCtrl.filteredDevicesList);
+		$scope.tourCtrlData.filteredDevicesList = calculateDistance($scope.tourCtrlData.filteredDevicesList);
 		
 		// Update Ionic View Array
-		updateIonicView($scope.bleDevicesCtrl.filteredDevicesList);
+		updateIonicView($scope.tourCtrlData.filteredDevicesList);
 		
-		//if(secondsLastViewUpdate + MIN_VIEW_UPDATE_INTERVAL < new Date().getTime() / 1000)
-		{
+		//if(secondsLastViewUpdate + tourCtrlConfig.MIN_VIEW_UPDATE_INTERVAL < new Date().getTime() / 1000)
+		//{
 			secondsLastViewUpdate = new Date().getTime() / 1000;
-			$scope.$apply();
-		}
+			$scope.$digest();
+		//}
 		
 	};
 	
+
 	
 	// Start Scanning
 		//TODO: Automatically start scanner here
 	
 	//TODO: Retreive All Beacon List from Server
+	var init = function() {
+		// Register for Updates when Beacons are scanned
+		bleScannerChannel.onFoundBleDevice($scope, onFoundDeviceHandler );
+	}
 	
-	// Register for Updates when Beacons are scanned
-	bleScannerChannel.onFoundBleDevice($scope, onFoundDeviceHandler );
+	init();
+	
 
 }]);
