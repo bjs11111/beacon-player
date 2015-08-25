4b71fcf72a446212ae693b30b17e641cd99286bc
Lukas Obletter da Cudan <developers@skirentalresorts.com>
2015-08-25 15:56:51 +0200

    Solved issue with QR Scanner on ipad

--- www/app/common/directives/qr-scanner/qr-scanner.js
+++ www/app/common/directives/qr-scanner/qr-scanner.js
@@ -20,19 +20,25 @@ qrScanner.directive('sitQrScanner', [ '$ionicPlatform', '$cordovaBarcodeScanner'
 	    		//defaults
 	    		scope.successCallback = (scope.successCallback) ? scope.successCallback : generalService.qrSuccessCallback;
 	    		scope.errorCallback = (scope.errorCallback) ? scope.errorCallback : generalService.qrErrorCallback ;
+	    		scope.isQrScannerOpen=false;
 	    	}
 	    	
 	    	//ng-click callback of directive
 	    	scope.scannQrCode = function () {
+	    		if(scope.isQrScannerOpen==false){
+	    			scope.isQrScannerOpen=true;
 		    		$ionicPlatform.ready(function() {
 		    			$cordovaBarcodeScanner
 		    				.scan()
 				    			.then(function(barcodeData) {
+				    				scope.isQrScannerOpen=false;
 				    				scope.successCallback(barcodeData);
 				    			}, function(error) {
+				    				scope.isQrScannerOpen=false;
 				    				scope.errorCallback(error);
 				    			});	
 		    		});
+	    		}
 	    	};
 	    	
 	    	init();
