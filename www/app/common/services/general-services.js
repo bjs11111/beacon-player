d250a0b25d5e0e0320db3f41f5f5cd8c0de3dcac
Lukas Obletter da Cudan <developers@skirentalresorts.com>
2015-09-01 17:09:48 +0200

    Moved Bar in IAB to the top

--- www/app/common/services/general-services.js
+++ www/app/common/services/general-services.js
@@ -302,30 +302,33 @@ generalServices.factory('generalService', ['$rootScope', '$timeout', '$filter', 
 			$cordovaInAppBrowser.executeScript({ 
 				//file: 'app/common/services/assets/css/iab.js',
 				code: 
-	 		    	   "var link_buttonText = document.createTextNode('Scan for new content');\
+	 		    	   "var link_buttonText = document.createTextNode('< Back');\
 	 		    		var link_button = document.createElement('a');\
 	 		    		link_button.setAttribute('onclick', \"window.close();\");\
 	 		    		link_button.setAttribute('href', 'http://www.beacon-player.at/close-iab.action');\
 	 			    	link_button.id = 'iba-close-button';\
-	 		    		link_button.style.fontSize = '14px';\
+	 		    		link_button.style.fontSize = '20px';\
+	 		    		link_button.style.height = '50px';\
 	 		    		link_button.style.textDecoration = 'none';\
-	 		    		link_button.style.color = '#fff';\
+	 		    		link_button.style.color = '#444';\
 	 		    		link_button.style.lineHeight = '20px';\
-	 		    		link_button.style.textAlign	= 'center';\
+	 		    		link_button.style.textAlign	= 'left';\
 	 		    		link_button.style.verticalAlign = 'middle';\
-	 		    		link_button.style.backgroundColor = '#ef473a';\
+	 		    		link_button.style.backgroundColor = '#f8f8f8';\
+	 		    		link_button.style.borderBottom = '1px solid #b2b2b2';\
 	 		    		link_button.style.margin = '0px';\
-	 		    		link_button.style.padding = '12px';\
+	 		    		link_button.style.padding = '15px';\
 	 		    		link_button.style.display = 'block';\
 	 		    		link_button.appendChild(link_buttonText);\
 	 		    		var footer = document.createElement('div');\
 	 		    		footer.style.position = 'fixed';\
-	 		    		footer.style.bottom = 0;\
+	 		    		footer.style.top = 0;\
 	 		    		footer.style.left = 0;\
 	 		    		footer.style.right = 0;\
 	 		    		footer.style.zIndex = 2147483647;\
 	 		    		footer.appendChild(link_button);\
-	 		    		document.body.appendChild(footer);"
+	 		    		document.body.appendChild(footer);\
+						document.body.style.margin = '50px 0px 0px 0px';"
 			});
 			
 			$cordovaInAppBrowser.insertCSS({ 
