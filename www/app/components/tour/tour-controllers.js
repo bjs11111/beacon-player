d683160cd313f2320097a940f29162f44ce81c08
Lukas Obletter da Cudan <developers@skirentalresorts.com>
2015-09-01 11:18:04 +0200

    NFC Tests, still needs to be finished

--- www/app/components/tour/tour-controllers.js
+++ www/app/components/tour/tour-controllers.js
@@ -19,8 +19,8 @@ tourControllers.constant("tourCtrlConfig", {
 
 /* Tour Controllers */
 tourControllers.controller( 'tourCtrl', 
-		['$scope', '$filter', 'tourCtrlConfig', 'generalService', 'bleScannerChannel','serverBeaconStore', 'beaconAPIChannel', 'bleScannerChannel',
-function( $scope,   $filter,   tourCtrlConfig,   generalService,   bleScannerChannel,  serverBeaconStore,   beaconAPIChannel,   bleScannerChannel  ) {
+		['$scope', '$filter', 'tourCtrlConfig', 'generalService', 'bleScannerChannel','serverBeaconStore', 'beaconAPIChannel', 'bleScannerChannel','nfc'
+function( $scope,   $filter,   tourCtrlConfig,   generalService,   bleScannerChannel,  serverBeaconStore,   beaconAPIChannel,   bleScannerChannel, 'nfc'  ) {
 	
 	var secondsLastViewUpdate = 0;
 	var knownBeaconsFilter = $filter('knownBeaconsFilter');
@@ -259,6 +259,32 @@ function( $scope,   $filter,   tourCtrlConfig,   generalService,   bleScannerCha
 		);
 		
 		startUpdateInterval(1000);
+		
+		
+		// Register NFC Listener
+	    nfc.addNdefListener (
+	        function (nfcEvent) {
+	            var tag = nfcEvent.tag,
+	                ndefMessage = tag.ndefMessage;
+
+	            // dump the raw json of the message
+	            // note: real code will need to decode
+	            // the payload from each record
+	            alert(JSON.stringify(ndefMessage));
+
+	            // assuming the first record in the message has 
+	            // a payload that can be converted to a string.
+	            alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+	        }, 
+	        function () { // success callback
+	            alert("Waiting for NDEF tag");
+	        },
+	        function (error) { // error callback
+	            alert("Error adding NDEF listener " + JSON.stringify(error));
+	        }
+	    );
+		
+		
 	}
 	
 	init();
